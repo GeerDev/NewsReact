46ae9702d6f20a87b67367b4170246e1bf737659
Gerahara <gerfersie@gmail.com>
2022-02-19 16:46:42 +0100

    First try in Form Layout :)

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+import './Form.css';
+
 const Form = () => {
 
   let navigate = useNavigate();
 
   const [data, setData] = useState({
         name: "",
-        email: ""
+        title: "",
+        description: "",
+        place: "",
+        image: "",
+        hastags: []
       });
 
   const handleInputChange = (event) => {
@@ -19,7 +25,10 @@ const Form = () => {
 
   const handleSubmit = (event) => {
         event.preventDefault();
-        localStorage.setItem("dataForm", JSON.stringify(data))
+
+        let save = JSON.parse(localStorage.getItem('dataForm')) || [];
+        save.push(data)
+        localStorage.setItem("dataForm", JSON.stringify(save))
 
         setTimeout(() => {
             navigate("/list");
@@ -27,21 +36,54 @@ const Form = () => {
       };
     
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        placeholder="name"
-        onChange={handleInputChange}
-        name="name"
-      />
-      <input
-        type="email"
-        placeholder="email"
-        onChange={handleInputChange}
-        name="email"
-      />
-      <button type="submit">Enviar</button>
-</form>
+    <section className='form animate__animated animate__jackInTheBox'> 
+      <form onSubmit={handleSubmit} className='form_body'>
+          <h2>Fill in these details:</h2>
+          <input
+            type="text"
+            placeholder="Name"
+            onChange={handleInputChange}
+            name="name"
+          />
+          <input
+            type="text"
+            placeholder="Title"
+            onChange={handleInputChange}
+            name="title"
+          />
+          <textarea
+            type="text"
+            placeholder="Description"
+            onChange={handleInputChange}
+            name="description"
+          />
+          <div className="form_group">
+            <input
+              type="text"
+              placeholder="Place"
+              onChange={handleInputChange}
+              name="place"
+            />
+            <input
+              type="file"
+              placeholder="Add a image"
+              onChange={handleInputChange}
+              name="image"
+            />
+          </div>
+          <input
+            type="text"
+            placeholder="Hastags"
+            onChange={handleInputChange}
+            name="hastags"
+            className="hastags"
+          />
+          <button type="submit">Send</button>
+      </form>
+      <div className="form_ilustration">
+      </div>
+    </section>
+
   )
 }
 
