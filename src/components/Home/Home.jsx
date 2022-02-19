d982e7953ad34352b61716a45c1092480e61d034
Gerahara <gerfersie@gmail.com>
2022-02-19 12:56:05 +0100

    Typewriter Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,14 @@
 import './Home.css';
 
+import logo from '../../assets/Logo.png'
+
 const Home = () => {
     return (
-      <div className="home">
-         <h1>Welcome to <span>Food News</span></h1>
-         <p>Description Food News</p>
-         <button>Create a new</button>
+      <div className="home animate__animated animate__fadeIn">
+         <img src={logo} alt="Logo Food News"/>
+         <h1>Welcome to <span className='typewriter'>Food News</span></h1>
+         <p>The best page to create and see news about food</p>
+         <button>Create a new news</button>
       </div>
     )
   }
